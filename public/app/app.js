5ac3fad9f54476542bce41935d19741e7e95dc4d
Matt Latham <latham75@gmail.com>
2016-08-02 13:10:05 -0500

    started adding firebase, html

---
+++ public/app/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var app = angular.module("ChartApp", ['ngRoute'])
+   .constant('FirebaseURL', "https://albumchart-f528c.firebaseio.com/");
+
+app.config(function($routeProvider, FBCreds) {
+      let authConfig = {
+         apiKey: FBCreds.apiKey,
+         authDomain: FBCreds.authDomain
+      };
+      // firebase.initializeApp(authConfig);
+
+      // $routeProvider.
---
+++ public/app/factories/AuthFactory.js
@@ -0,0 +1,39 @@
+"use strict";
+
+app.factory("AuthFactory", function() {
+
+
+   let currentUserId = null;
+   let provider = new firebase.auth();
+
+   firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+         currentUserId = user.uid;
+      } else {
+         currentUserId = null;
+         console.log("not logged in");
+      }
+   });
+
+   let authWithProvider = function() {
+      return firebase.auth().signInWithPopup(provider);
+   };
+
+   let isAuthenticated = function() {
+      return (currentUserId) ? true : false;
+   };
+
+   let getUser = function() {
+      return currentUserId;
+   };
+
+   let logout = function() {
+      currentUserId = null;
+   };
+
+
+   return {
+      authWithProvider, isAuthenticated, getUser, currentUserId, logout
+   };
+
+});
