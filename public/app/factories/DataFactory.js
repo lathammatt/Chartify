d889a76028292dc6549138f0bbdba0521f5d883d
Matt Latham <latham75@gmail.com>
2016-08-03 15:53:37 -0500

    data functions

--- public/app/factories/DataFactory.js
+++ public/app/factories/DataFactory.js
@@ -5,13 +5,15 @@ app.factory("DataTransfer", function(FireaseURL, $q, $http, AuthFactory) {
    let getTopSongs = function() {
       let albums = [];
       return $q(function(resolve, reject) {
-         $http.get(`"https://api.spotify.com/v1/me/top/tracks?time_range=${timeframe}" -H "Accept: application/json" -H "Authorization: Bearer${Oauth_token}`)
+         // $http.get(`"https://api.spotify.com/v1/me/top/tracks?time_range=${timeframe}" -H "Accept: application/json" -H "Authorization: Bearer${Oauth_token}`)
+         $http.get("test2.json")
             .success(function(returnedData) {
                let songlist = returnedData;
                for (var i = 0; i < songlist.length; i++) {
                   albums.push(items[i].album.id);
+                  console.log("albums", items[i].album.id);
                }
-               resolve(albums);
+               resolve(getAlbums(albums));
             })
             .error(function(error) {
                reject(error);
@@ -21,7 +23,29 @@ app.factory("DataTransfer", function(FireaseURL, $q, $http, AuthFactory) {
 
 
    let getAlbums = function(albumList) {
-
+      let query = albumList.join(",");
+      let chartdata = [];
+      return $q(function(resolve, reject) {
+         $http.get(`https://api.spotify.com/v1/albums/?ids=${query}`);
+            .success(function(returnedData) {
+               for (var i = 0; i < returnedData.length; i++) {
+                  let obj = {};
+                  obj.type = albums[i].album_type;
+                  obj.artistname = albums[i].artists.name;
+                  obj.id = albums[i].id;
+                  obj.artwork = albums[i].images[0].url;
+                  obj.albumname = albums[i].name;
+                  obj.release = albums[i].release_date;
+                  obj.songs = {};
+                  for (var j = 0; j < returnedData.albums.tracks.length; j++) {
+                     obj.songs.number = albums[i].tracks.items[j].track_number;
+                     obj.songs.name = albums[i].tracks.items[j].name;
+                  }
+                  chartdata.push(obj);
+               }
+            })
+         resolve(chartdata)
+      });
    };
 
 
